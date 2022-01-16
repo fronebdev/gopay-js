a8a7c56a6e16f7e2fc0859a23a340fd7ad9894c6
0ndras3k <lordklapka@gmail.com>
2022-01-17 00:49:50 +0100

    pre-publish

--- README.md
+++ README.md
@@ -1,3 +1,65 @@
-# gopay-js
+<p align="center">
+ <img width="250" src="https://cdn.myshoptet.com/usr/www.hobbychef.cz/user/documents/upload/GoPay-logo-varianta-A-PANTONE.png" alt="GPlogo"/>
+</p>
 
+<h1 align="center">gopay-js</h1>
+
+<div align="center">
 The only functional library for GoPay payment gateway.
+</div>
+
+## ⚡️ Getting Started
+
+### Installation
+
+```bash
+$ npm i @fronebdev/gopay-js
+# or if you are using Yarn
+$ yarn add @fronebdev/gopay-js
+```
+
+# 🖇 Documentation
+
+## Tree
+
+- init
+- getToken()
+
+### Initialization
+
+```ts
+const gp = new goPay({
+  log: true,
+  enviroment: "test",
+  credentials: {
+    clientID: "1390489727",
+    clientSecret: "9G9ha7Qa",
+  },
+});
+```
+
+## Methodes
+
+All methodes included in library.
+
+### getToken()
+
+the getToken() method returns all the necessary keys, to work with GoPay here is example.:
+
+```json
+{
+  "token_type": "bearer",
+  "access_token": "token",
+  "expires_in": 1800,
+  "refresh_token": "token"
+}
+```
+
+## 🙅🏿‍♂️ Used OSS
+
+- Chalk
+  - https://github.com/chalk/chalk
+
+# ⭐️ License
+
+gopay-js is licensed under MIT. See the LICENSE file.
--- src/factory/goPay.ts
+++ src/factory/goPay.ts
@@ -25,27 +25,30 @@ class goPay {
       this.url = "https://gate.gopay.cz/api";
     }
 
-    if(log)
-      console.log(with_gopay("Initializating..."));
+    if (log) console.log(with_gopay("Initializating..."));
   }
 
   async getToken() {
-      const params = new URLSearchParams();
-      params.append("grant_type", "client_credentials")
-      params.append("scope", "payment-create")
+    const params = new URLSearchParams();
+    params.append("grant_type", "client_credentials");
+    params.append("scope", "payment-create");
 
-      const res = await axios({
-        url: this.url + "/oauth2/token",
-        method: "POST",
-        headers: {
-          "Accept": "application/json",
-          "Content-Type": "application/x-www-form-urlencoded",
-          "Authorization": "Basic " + new Buffer(this.credentials.clientID + ":" + this.credentials.clientSecret).toString("base64"),
-        },
-        data: params,
-      })
+    const res = await axios({
+      url: this.url + "/oauth2/token",
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/x-www-form-urlencoded",
+        Authorization:
+          "Basic " +
+          new Buffer(
+            this.credentials.clientID + ":" + this.credentials.clientSecret
+          ).toString("base64"),
+      },
+      data: params,
+    });
 
-      return res.data
+    return res.data;
   }
 }
 
