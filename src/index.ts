fe06a9ddf472586d20f10873a1b1937824b36fbf
0ndras3k <lordklapka@gmail.com>
2022-01-17 01:04:02 +0100

    refractor: removed tests

--- src/index.ts
+++ src/index.ts
@@ -10,16 +10,3 @@ import goPay from "./factory/goPay";
 import { with_gopay } from "./helpers";
 
 export * from "./factory/goPay";
-
-const gp = new goPay({
-  log: true,
-  enviroment: "test",
-  credentials: {
-    clientID: "1390489727",
-    clientSecret: "9G9ha7Qa",
-  },
-});
-
-gp.getToken();
-
-console.log(with_gopay("Starting ..."));
